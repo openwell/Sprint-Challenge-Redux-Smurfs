a56a6f68f96247ac7ea5d7a3d4b6f27338c7c7f3
openwell <timileyinojo@yahoo.com>
2019-06-21 10:00:18 +0100

    App packages setup and getSmurfs functionality

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -2,6 +2,12 @@
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
+import axios from 'axios'
+export const ADDSMURF = "ADDSMURF";
+export const GETSMURFS = "GETSMURFS";
+export const UPDATESMURF = "UPDATESMURF";
+export const DELETESMURF = "DELETESMURF";
+export const FAILURE = "FAILURE";
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
@@ -13,3 +19,29 @@
    U - updateSmurf
    D - deleteSmurf
 */
+export const getSmurfs = () => dispatch => {
+  axios
+    .get("http://localhost:3333/smurfs")
+    .then(res => {
+      console.log(res.data)
+      dispatch(addSmurf(res.data));
+    })
+    .catch(err => {
+      dispatch(failure(err.message));
+    });
+};
+
+export const addSmurf = res => {
+  return {
+    type: ADDSMURF,
+    val: res
+  };
+};
+export const failure = err => {
+  return {
+    type: FAILURE,
+    val: err
+  };
+};
+
+
